b57559c5b73f40837dbe2ab3c9d17b8897e1b92b
Abdullah <abdulluhodulate@gmail.com>
2023-07-01 21:59:33 +0200

    carousel function

--- index.js
+++ index.js
@@ -2,9 +2,56 @@ const ham = document.querySelector(".ham");
 const mobileNav = document.querySelector(".toggle");
 
 const handleToggler = () => {
-	mobileNav.classList.toggle("hidden");
-	mobileNav.classList.toggle("flex");
+  mobileNav.classList.toggle("hidden");
+  mobileNav.classList.toggle("flex");
 };
 
 ham.addEventListener("click", handleToggler);
 mobileNav.addEventListener("click", handleToggler);
+
+const buttons = document.querySelectorAll("[data-carousel-btn]");
+const nextBtn = document.querySelector('[data-carousel-button="next"]');
+const slideContainer = document.querySelector("[data-carousel] [data-slides]");
+let initialX = null;
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const offset = btn.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = btn.closest("[data-carousel]").querySelector("[data-slides");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
+
+setInterval(() => {
+  nextBtn.click();
+}, 7000);
+
+slideContainer.addEventListener("touchstart", (e) => {
+  initialX = e.touches[0].clientX;
+});
+
+slideContainer.addEventListener("touchend", (e) => {
+  const finalX = e.changedTouches[0].clientX;
+  const deltaX = finalX - initialX;
+
+  let prevBtn, nextBtn;
+
+  prevBtn = document.querySelector('[data-carousel-button="prev"]');
+  nextBtn = document.querySelector('[data-carousel-button="next"]');
+
+  if (deltaX > 0) {
+    // console.log(prevBtn.click());
+    prevBtn.click();
+  } else if (deltaX < 0) {
+    // console.log(nextBtn.click);
+    nextBtn.click();
+  }
+});
