5fa19c638b4e2235fef0c5e62f11e918d7058304
dmystical-coder <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2023-07-08 13:22:58 +0100

    Deliver trending milestones

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,5 +15,5 @@ function showSlides() {
 
     slides[slideIndex-1].style.display = "block";  
     
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
   }
