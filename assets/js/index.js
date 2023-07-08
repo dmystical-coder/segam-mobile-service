fe4806f3218aa65e0f6c87d8b4438e304c8d2229
dmystical-coder <abdulsalam.baruwa190541028@st.lasu.edu.ng>
2023-07-08 14:22:19 +0100

    Control when audio plays

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,14 +6,44 @@ const numberOfSlides = slides.length;
 showSlides();
 
 function showSlides() {
-    let i;
-    for (i = 0; i < numberOfSlides; i++) {
-      slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > numberOfSlides) {slideIndex = 1;}    
-
-    slides[slideIndex-1].style.display = "block";  
-    
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  let i;
+  for (i = 0; i < numberOfSlides; i++) {
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > numberOfSlides) { slideIndex = 1; }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+
+document.addEventListener('DOMContentLoaded', controlPlay());
+
+function controlPlay() {
+  var audio = document.getElementById("myAudio");
+  audio.currentTime = 0;
+  audio.play();
+  console.log(audio.currentTime); // this is to check the currentTime in the console log
+
+  window.addEventListener('scroll', function () {
+    let audioPosition = audio.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight;
+
+    this.setInterval(function () {
+      if (audioPosition < 0 || audioPosition > windowHeight) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    }, 1000);
+  })
+  // the below setInterval is to check the currentTime is greater than 56 or not in every 1 second
+  setInterval(function () {
+    if (audio.currentTime > 30) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }, 1000);
+
+}
